ad7564d38449aba5f292610b46740fe087df447d
Subham Saha <subham@Subhams-MacBook-Pro.local>
2024-01-07 02:57:44 +0530

    completed

---
+++ android/app/src/main/java/bio/jap/app/MainActivity.java
@@ -0,0 +1,5 @@
+package bio.jap.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,11 +1,26 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'bio.jap.app',
   appName: 'JAP',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      launchFadeOutDuration: 0,
+      backgroundColor: "#ddf8e8",
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true,
+      // layoutName: "launch_screen",
+      // useDialog: true,
+    },
   }
 };
 
